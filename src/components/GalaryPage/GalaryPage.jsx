8f54ab15ba725092efeb6a58881a7a722c009b8b
MD IRFAN RAJ <mdirfanraj88.omega@gmail.com>
2024-07-11 17:34:02 +0530

    Add files via upload

---
+++ src/components/GalaryPage/GalaryPage.jsx
@@ -0,0 +1,18 @@
+// src/components/PeoplePage/PeoplePage.jsx
+import Hero from "../HeroSection/Hero.jsx";
+import Nav from "../NavBar/Nav.jsx";
+import Galary from "../Galary/Galary.jsx";
+import Footer from "../Footer/Footer.jsx";
+
+const GalaryPage = () => {
+  return (
+    <>
+      <Hero />
+      <Nav />
+      <Galary />
+      <Footer />
+    </>
+  );
+};
+
+export default GalaryPage;
