3d689f0601a4480189db6c06b02bfbc205c155bc
Bhivanshu45 <bhivanshulawaniya6@gmail.com>
2024-07-14 23:18:24 +0530

    update Galary.jsx/.css

--- src/components/Galary/Galary.jsx
+++ src/components/Galary/Galary.jsx
@@ -6,161 +6,161 @@ const Galary = () => {
       <h1 class="hea"><strong>GALLERY</strong> </h1>
     <h2 class="hea">UNNATI SAMAROH</h2>
     <div class="container">
-        <div class="card">
+        <div class="g-card">
             <img src="samaroh .JPG"   alt="man"  />
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="samaroh 2.JPG"   alt="man"  /> 
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="samaroh6.JPG"   alt="man"  /> 
         </div>
     </div>
     <div class="container">
-        <div class="card">
+        <div class="g-card">
             <img src="samaroh5.JPG"   alt="man"  />
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="samaroh4.JPG"   alt="man"  /> 
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="samaroh7.JPG"   alt="man"  /> 
         </div>
     </div>
     <div class="container">
-        <div class="card">
+        <div class="g-card">
             <img src="unnati team 1.jpeg"   alt="man"  />
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="samaroh-3.jpeg"   alt="man"  /> 
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="unnati team 2.jpeg"   alt="man"  /> 
         </div>
     </div>
 
     <h2 class="hea">JOURNEY START...</h2>
     <div class="container">
-        <div class="card">
+        <div class="g-card">
             <img src="new beginning 2.jpg"   alt="man"  />
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="new beginning.jpg"   alt="man"  /> 
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="startting day.jpeg"   alt="man"  /> 
         </div>
     </div>
     <div class="container">
-        <div class="card">
+        <div class="g-card">
             <img src="startting day 2.jpeg"   alt="man"  />
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="team.jpeg"   alt="man"  /> 
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="team 2.jpeg"   alt="man"  /> 
         </div>
     </div>
     <div class="container">
-        <div class="card">
+        <div class="g-card">
             <img src="../../../cl"   alt="man"  />
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="class2.jpeg"   alt="man"  /> 
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="class 3.jpeg"   alt="man"  /> 
         </div>
     </div>
     // ../../../
     <div class="container">
-        <div class="card">
+        <div class="g-card">
             <img src="class4.jpg"   alt="man"  />
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="class5.jpg"   alt="man"  /> 
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="class6.jpg"   alt="man"  /> 
         </div>
     </div>
     <div class="container">
-        <div class="card">
+        <div class="g-card">
             <img src="class7.jpg"   alt="man"  />
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="class8.jpg"   alt="man"  /> 
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="class9.jpg"   alt="man"  /> 
         </div>
     </div>
     <div class="container">
-        <div class="card">
+        <div class="g-card">
             <img src="class10.jpg"   alt="man"  />
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="class11.jpg"   alt="man"  /> 
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="class12.jpeg"   alt="man"  /> 
         </div>
     </div>
     <h2 class="hea">TEAM MEETING</h2>
     <div class="container">
-        <div class="card">
+        <div class="g-card">
             <img src="meeting2.jpg"   alt="man"  />
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="meeting1.jpg"   alt="man"  /> 
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="meeting3.jpg"   alt="man"  /> 
         </div>
     </div>
     <div class="container">
-        <div class="card">
+        <div class="g-card">
             <img src="meeting4.jpg"   alt="man"  />
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="meeting5.jpg"   alt="man"  /> 
         </div>
        
     </div>
     <h2 class="hea">TEST AND PRIZE DISTRIBUTION</h2>
     <div class="container">
-        <div class="card">
+        <div class="g-card">
             <img src="test1.jpg"   alt="man"  />
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="test2.jpg"   alt="man"  /> 
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="prize1.jpg"   alt="man"  /> 
         </div>
         
     </div>
     <div class="container">
-        <div class="card">
+        <div class="g-card">
             <img src="prize4.jpeg"   alt="man"  />
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="prize2.jpg"   alt="man"  /> 
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="prize3.jpg"   alt="man"  /> 
         </div>
         
     </div>
     <div class="container">
-        <div class="card">
+        <div class="g-card">
             <img src="prize7.jpg"   alt="man"  />
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="prize6.jpg"   alt="man"  /> 
         </div>
-        <div class="card">
+        <div class="g-card">
             <img src="prize8.jpg"   alt="man"  /> 
         </div>
         
